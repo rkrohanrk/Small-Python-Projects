7a1797b216f474e1eb2c4d7f331cbca26c31e213
Rohan <rk359979@gmail.com>
2022-04-09 15:35:57 +0530

    code using smtplib

---
+++ mail_sending.py
@@ -0,0 +1,16 @@
+import smtplib
+from socket import MsgFlag
+server=smtplib.SMTP('smtp.gmail.com','587')
+
+# connection starting with transport layer security.
+server.starttls()
+
+server.login('rk359979@gmail.com','type in your password😄') 
+subject="send using python"
+body="this mail is send using python"
+
+msg="subject:{}\n\n\n.".format(subject,body)
+maillist=["rkumar2_be19@thapar.edu","rohan16091@gmail.com"]
+server.sendmail("rk359979@gmail.com",maillist,msg)
+print("Send successfully...!!")
+server.quit()
